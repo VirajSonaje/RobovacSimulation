80d6fb2454209124828fb45b86f5790f652ff076
VirajSonaje <virajsonaje@gmail.com>
2021-11-26 20:19:20 -0700

    Update transducer.java

--- transducer.java
+++ transducer.java
@@ -1,21 +1,16 @@
 package Component.RobovacSimulation;
 
+import GenCol.Queue;
+import GenCol.entity;
+import model.modeling.message;
 import view.modeling.ViewableAtomic;
 
-import java.awt.Color;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.swing.text.html.parser.Entity;
-
-import GenCol.doubleEnt;
-import GenCol.entity;
-
-
-import model.modeling.content;
-import model.modeling.message;
-
 public class transducer extends ViewableAtomic {
+	
+	double clock =0;
+	Queue<String> operations = new Queue<String>();
+	Battery b1, b2;
+	
 	public transducer() {
 		this("transducer");
 	}
@@ -26,22 +21,18 @@ public class transducer extends ViewableAtomic {
 		addOutport("Battery2");
 		addInport("Start");
 		addInport("Stop");
-		addInport("b1");
-		addInport("b2");
-		
-		bat1 = new HashMap();
-		bat2 = new HashMap();
+		addInport("In");
 		
 		addTestInput("Start", new entity("start"));
 		addTestInput("Stop", new entity("stop"));
 		addTestInput("Battery1", new entity("val"));
 		addTestInput("Battery2", new entity("val"));
-		initialize();
 		
-		setBackgroundColor(Color.pink);
-		
+		b1 = new Battery("Li-ion");
+		b2 = new Battery("NiCa");
+
 	}
-	
+
 	@Override
 	public void initialize() {
 		super.initialize();
@@ -50,47 +41,45 @@ public class transducer extends ViewableAtomic {
 	}
 	
 	@Override
-	public void delint() {
+	public void deltint() {
+		// TODO Auto-generated method stub
+		super.deltint();
 		clock = clock + sigma;
 		passivate();
 		show_state();
+	
 	}
 	
+
 	@Override
-	public void delext(double e, battery b) {
+	public void deltext(double e, message x) {
 		Continue(e);
-		Entity val;
-		if(messageOnPort(b,"b1",1)) {
-			val = b.getValOnPort("b1",1);
-			bat1.put(val.getName(), new doubleEnt(clock));
+		entity val;
+		for(int i=0; i<x.getLength(); i++) {
+			if(messageOnPort(x,"In",i)) {
+				val = x.getValOnPort("In",i);
+				operations.add(val.getName());
+			}	
 		}
-		if(messageOnPort(b,"b2",1)) {
-			val = b.getValOnPort("b2",1);
-			bat2.put(val.getName(), new doubleEnt(clock));
-		}
+		
 	show_state();
 	}
 		
 	
-	public battery out(battery) {
-		battery b = new battery();
-		content con1 = makeContent("battery1", new entity(" "+batteryDissipation(battery1)));
-		content con2 = makeContent("battery2",new entity(" "+batteryDissipation(battery2)));
-		b.add(con1);
-		b.add(con2);
-		return b;
+	public message out() {
+		message m = new message();
+//		content con1 = makeContent("battery1", new entity(" "+batteryDissipation(battery1)));
+//		content con2 = makeContent("battery2",new entity(" "+batteryDissipation(battery2)));
+//		m.add(con1);
+//		m.add(con2);
+		return m;
 	}
 	
-	public double batteryDissipation(battery) {
-		double bt1 = 100;
-		double bt2 = 100;
-		if(!b1.isEmpty()) {
-			bt1 = bt1 - (a*bt1);//a is the dissipation percent from a bot component
-			return bt1;
-		}
-		if(!b2.isEmpty()) {
-			bt2 = bt2 - (a*bt2);//a is the dissipation percent from a bot component
-			return bt2;	
-		}
+	private void show_state() {
+		// TODO Auto-generated method stub
+		System.out.println("Current battery Level for "+ b1.getBatteryType() +": " + b1.getBatteryLevel());
+		System.out.println("Current battery Level for "+ b2.getBatteryType() +": " + b2.getBatteryLevel());
 	}
+	
+
 }
